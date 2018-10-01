c50a376216cfc0249246110ce20424e04b9ad51d
Fabs <fnascimento@mesosphere.com>
2018-10-01 21:15:17 +0200

    Add returncode to dcos_cli.exec_command

--- tests/test_dcos_cli.py
+++ tests/test_dcos_cli.py
@@ -6,7 +6,7 @@ from dcos_test_utils import dcos_cli
 
 def test_exec_command(caplog):
     cli = dcos_cli.DcosCli('')
-    stdout, stderr = cli.exec_command(
+    stdout, stderr, returncode = cli.exec_command(
         ['/bin/sh', '-c', 'echo "hello, world!"']
     )
     assert stdout == 'hello, world!\n'
@@ -14,11 +14,14 @@ def test_exec_command(caplog):
     assert any(rec.message.startswith('CMD:') for rec in caplog.records)
     assert any(rec.message.startswith('STDOUT:') for rec in caplog.records)
     assert any(rec.message.startswith('STDERR:') for rec in caplog.records)
+    assert returncode == 0
 
 
 def test_exec_command_fail(caplog):
     cli = dcos_cli.DcosCli('')
     with pytest.raises(subprocess.CalledProcessError):
-        cli.exec_command(['/bin/sh', '-c', 'does-not-exist'])
+        _stdout, _stderr, returncode = cli.exec_command(['/bin/sh', '-c', 'does-not-exist'])
+        assert returncode != 0
+
     assert any(rec.message.startswith('CMD:') for rec in caplog.records)
     assert any(rec.message.startswith('STDERR:') for rec in caplog.records)
