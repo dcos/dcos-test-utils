1dc838b11b5f7b4f609e8e39d60630b26fc3268c
jbarrick@mesosphere.com <jbarrick@mesosphere.com>
2019-01-25 12:08:52 -0800

    Add plugin for collecting xfailflakes

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,2 +1,33 @@
+import json
+import pytest
+
+
 def test_dcos_api_session_factory_fixture_imported(dcos_api_session_factory):
     pass
+
+
+@pytest.mark.xfailflake(
+    jira='DCOS-1337',
+    reason='A reason',
+    since='2019-01-25'
+)
+def test_xfailflake():
+    # Test always fails, but the test suite should still pass.
+    assert False
+
+
+def test_xfailflake_report():
+    # This should be written out by the collect process.
+    report = json.load(open('xfailflake.json'))
+    assert report == [
+        {
+            'module': 'test_plugin',
+            'name': 'test_xfailflake',
+            'path': __file__,
+            'xfailflake': {
+                'jira': 'DCOS-1337',
+                'reason': 'A reason',
+                'since': '2019-01-25'
+            }
+        }
+    ]
